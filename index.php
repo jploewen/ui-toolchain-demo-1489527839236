650475c9d4ec3403b71d3feda9f63752c4567bc3
jploewen <jploewen@us.ibm.com>
2017-03-18 22:04:18 -0400

    Add ping pong

--- index.php
+++ index.php
@@ -97,10 +97,11 @@ function orderItem(itemID){
   
      <!-- Add large images, uncomment the next 4 lines to create a response time alarm in BAM -->
 	
-	<img id="mynewlog" src="http://upload.wikimedia.org/wikipedia/commons/2/26/Invalidenpark%2C_Berlin-Mitte%2C_360x180%2C_160326%2C_ako.jpg" height="25%" width="25%" alt="Oops">
-	<img src="http://upload.wikimedia.org/wikipedia/commons/2/26/Invalidenpark%2C_Berlin-Mitte%2C_360x180%2C_160326%2C_ako.jpg" height="25%" width="25%" alt="Oops">
-	<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/%D0%9E%D0%B3%D0%BE%D0%BD%D0%B5%D0%BA_1917-32.pdf/page1-1285px-%D0%9E%D0%B3%D0%BE%D0%BD%D0%B5%D0%BA_1917-32.pdf.jpg" height="25%" width="25%" alt="Oops">
-	<img id="mylogo" src="http://upload.wikimedia.org/wikipedia/commons/0/07/Multi-use_water_bottle.JPG" height="25%" width="25%" alt="Oops">
+	<img id="berlin" src="http://upload.wikimedia.org/wikipedia/commons/2/26/Invalidenpark%2C_Berlin-Mitte%2C_360x180%2C_160326%2C_ako.jpg" height="25%" width="25%" alt="Oops">
+	<img id="berlin2" src="http://upload.wikimedia.org/wikipedia/commons/2/26/Invalidenpark%2C_Berlin-Mitte%2C_360x180%2C_160326%2C_ako.jpg" height="25%" width="25%" alt="Oops">
+	<img id="ubeck" src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/%D0%9E%D0%B3%D0%BE%D0%BD%D0%B5%D0%BA_1917-32.pdf/page1-1285px-%D0%9E%D0%B3%D0%BE%D0%BD%D0%B5%D0%BA_1917-32.pdf.jpg" height="25%" width="25%" alt="Oops">
+	<img id="bottles" src="http://upload.wikimedia.org/wikipedia/commons/0/07/Multi-use_water_bottle.JPG" height="25%" width="25%" alt="Oops">
+	<img id="pong" src="http://upload.wikimedia.org/wikipedia/commons/b/b6/Mesa_de_ping_pong%2C_Riesstr.%2C_M%C3%BAnich%2C_Alemania_2012-04-28%2C_DD_01.JPG"  height="25%" width="25%" alt="Oops">
 	
      <!-- Add large images, uncomment the previous 4 lines to create a response time alarm in BAM -->	
      <p>My favorite color is blue</p>
